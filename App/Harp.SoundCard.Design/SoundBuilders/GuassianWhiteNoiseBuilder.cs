01f4708e0b8c428daf4173fb4742bf206472e70e
Luís Teixeira <micboucinha@gmail.com>
2025-07-16 11:12:08 +0100

    Add custom signal builders

---
+++ App/Harp.SoundCard.Design/SoundBuilders/GuassianWhiteNoiseBuilder.cs
@@ -0,0 +1,57 @@
+﻿using NWaves.Signals.Builders.Base;
+using NWaves.Utils;
+using System;
+using System.Collections.Generic;
+using NWaves.Signals;
+
+namespace Harp.SoundCard.Design.SoundBuilders;
+
+public class GaussianWhiteNoiseBuilder : SignalBuilder
+{
+    private double _mean;
+    private double _stdDev;
+    private int? _seed;
+    private Random _rand;
+
+    public GaussianWhiteNoiseBuilder()
+    {
+        ParameterSetters = new Dictionary<string, Action<double>>
+        {
+            { "mean", param => _mean = param },
+            { "stddev, std", param => _stdDev = param },
+            {
+                "seed", param =>
+                {
+                    _seed = (int)param;
+                    _rand = _seed > 0 ? new Random(_seed.Value) : new Random();
+                }
+            }
+        };
+        _mean = 0.0;
+        _stdDev = 1.0;
+        _rand = new Random();
+    }
+
+    // Box-Muller transform for normal distribution
+    public override float NextSample()
+    {
+        var u1 = 1.0 - _rand.NextDouble();
+        var u2 = 1.0 - _rand.NextDouble();
+        var randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+        var value = (float)(_mean + _stdDev * randStdNormal);
+        
+        // Clamp the value to prevent overflow  
+        var min = (float)(_mean - _stdDev);
+        var max = (float)(_mean + _stdDev);
+
+        return Math.Clamp(value, min, max);
+    }
+
+    protected override DiscreteSignal Generate()
+    {
+        Guard.AgainstNonPositive(_stdDev, "Standard deviation");
+        _rand = _seed is > 0 ? new Random(_seed.Value) : new Random();
+
+        return base.Generate();
+    }
+}
---
+++ App/Harp.SoundCard.Design/SoundBuilders/UniformWhiteNoiseBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using NWaves.Signals;
+using NWaves.Signals.Builders.Base;
+using NWaves.Utils;
+
+namespace Harp.SoundCard.Design.SoundBuilders;
+
+public class UniformWhiteNoiseBuilder : SignalBuilder
+{
+    private double _min;
+    private double _max;
+    private int? _seed;
+    private Random _rand;
+
+    public UniformWhiteNoiseBuilder()
+    {
+        ParameterSetters = new Dictionary<string, Action<double>>
+        {
+            { "min, low, lo", param => _min = param },
+            { "max, high, hi", param => _max = param },
+            {
+                "seed", param =>
+                {
+                    _seed = (int)param;
+                    _rand = _seed > 0 ? new Random(_seed.Value) : new Random();
+                }
+            }
+        };
+        _min = -1.0;
+        _max = 1.0;
+        _rand = new Random();
+    }
+
+    public override float NextSample()
+    {
+        return (float)(_rand.NextDouble() * (_max - _min) + _min);
+    }
+
+    protected override DiscreteSignal Generate()
+    {
+        Guard.AgainstInvalidRange(_min, _max, "Upper amplitude", "Lower amplitude");
+        _rand = _seed is > 0 ? new Random(_seed.Value) : new Random();
+
+        return base.Generate();
+    }
+}
